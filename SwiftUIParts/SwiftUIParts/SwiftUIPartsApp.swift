a05a409098de3a33b7d9509cb551db9710eb16bb
Satoshi Moris Tagomori <tagomoris@gmail.com>
2023-03-31 12:20:51 +0900

    project and workspace init

---
+++ SwiftUIParts/SwiftUIParts/SwiftUIPartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPartsApp.swift
+//  SwiftUIParts
+//
+//  Created by 田籠聡 on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
