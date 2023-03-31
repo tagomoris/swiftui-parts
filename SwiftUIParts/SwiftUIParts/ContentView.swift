02decf8382f9fa34559e903b0b7b833e4b5423a8
Satoshi Moris Tagomori <tagomoris@gmail.com>
2023-03-31 12:37:37 +0900

    demo app

--- SwiftUIParts/SwiftUIParts/ContentView.swift
+++ SwiftUIParts/SwiftUIParts/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var geometry = ResizeableBoxGeometry(size: CGSize(width: 200, height: 200))
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Spacer()
+                Text("Size, width:\(geometry.size.width), height:\(geometry.size.height)")
+                Text("Origin X:\(geometry.origin.x), Y:\(geometry.origin.y)")
+                    .padding(.init(top: 0, leading: 0, bottom: 50, trailing: 0))
+            }
+            ResizeableBox(name: "selecting", geometry: $geometry)
         }
-        .padding()
     }
 }
 
